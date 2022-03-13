339438654d8bccd2137d05aee32d38746a34817c
Alan Sanchez <realalansan@gmail.com>
2022-03-13 15:22:24 -0700

    Adjust button component and styles

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,20 +1,40 @@
 import { MouseEventHandler, ReactNode } from 'react';
 
+import '../styles/components/_buttons.scss';
+
 interface Props {
-  onClick: MouseEventHandler;
-  classes: string;
-  styles: React.CSSProperties;
-  children: ReactNode;
+  onClick?: MouseEventHandler;
+  classes?: string;
+  styles?: React.CSSProperties;
+  rounded?: boolean;
+  link?: boolean;
+  sm?: boolean;
+  lg?: boolean;
+  xl?: boolean;
+  children?: ReactNode;
 }
 
 const Button: React.FC<Props> = ({
   onClick,
   classes,
+  rounded,
+  link,
+  sm,
+  lg,
+  xl,
   styles,
   children,
 }: Props) => {
   return (
-    <button className={`btn ${classes}`} style={styles} onClick={onClick}>
+    <button
+      className={`btn primary white ${sm ? 'sm' : ''} ${lg ? 'lg' : ''} ${
+        xl ? 'xl' : ''
+      } ${classes ? classes : ''} ${rounded ? 'rounded' : ''} ${
+        link ? 'link' : ''
+      }`}
+      style={styles}
+      onClick={onClick}
+    >
       {children}
     </button>
   );
