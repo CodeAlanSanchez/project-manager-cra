c11c19f1f2187d7132e6f815f83a6379a2553fe0
Alan Sanchez <realalansan@gmail.com>
2022-03-13 17:11:16 -0700

    Add react router to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import Layout from 'components/Layout/Layout';
+import { Home } from 'pages';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 const App: React.FC = () => {
-  return <Layout></Layout>;
+  return (
+    <Router>
+      <Layout>
+        <Route path="/" element={<Home />} />
+      </Layout>
+    </Router>
+  );
 };
 
 export default App;
