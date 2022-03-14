c6d477d9fc6f493cdbd42c61b175517e29b28d68
Alan Sanchez <realalansan@gmail.com>
2022-03-14 11:07:00 -0700

    Create home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,21 @@
 import 'styles/pages/_home.scss';
+import { Link } from 'react-router-dom';
+import Button from 'components/Button/Button';
 
 const Home: React.FC = () => {
-  return <div className={`home`}></div>;
+  return (
+    <div className={`home`}>
+      <div className="hero">
+        <h1 className="title">#1 Project Manager</h1>
+        <h5>The first for developers!</h5>
+        <Link to={'/register'}>
+          <Button rounded>Start Now</Button>
+        </Link>
+      </div>
+      <div className="features"></div>
+      <div className="goal"></div>
+    </div>
+  );
 };
 
 export default Home;
