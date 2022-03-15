569c080a51159aefd6dfd753c3a1f071b7e8aa91
Alan Sanchez <realalansan@gmail.com>
2022-03-14 17:14:21 -0700

    Add home page content

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,15 +5,52 @@ import Button from 'components/Button/Button';
 const Home: React.FC = () => {
   return (
     <div className={`home`}>
+      {/* Hero */}
       <div className="hero">
-        <h1 className="title">#1 Project Manager</h1>
-        <h5>The first for developers!</h5>
-        <Link to={'/register'}>
-          <Button rounded>Start Now</Button>
-        </Link>
+        <div className="description">
+          <h1 className="title">#1 Project Manager</h1>
+          <h5>The first for developers!</h5>
+          <Link to={'/register'}>
+            <Button rounded>Start Now</Button>
+          </Link>
+        </div>
+        <div className="imageContainer">
+          <img src="/collaboration.png" alt="collaboration" />
+        </div>
       </div>
-      <div className="features"></div>
-      <div className="goal"></div>
+      {/* Features */}
+      <div className="features">
+        <div className="item">
+          <h5>Track Bugs</h5>
+          <p>
+            Create, assign, complete. <br />
+            It&apos;s as simple as that!
+          </p>
+        </div>
+        <div className="item">
+          <h5>Set Goals</h5>
+          <p>
+            Add deadlines, create events, <br />
+            and finish on time!
+          </p>
+        </div>
+      </div>
+      <div className="item">
+        <h5>Finish</h5>
+        <p>
+          Finish current tasks then <br />
+          update everyone.
+          <br />
+        </p>
+      </div>
+      {/* Goals */}
+      <div className="goal">
+        <h3>Focus</h3>
+        <p>
+          Don&apos;t worry about managing your project. Focus on getting work
+          done. Sign up and start now!
+        </p>
+      </div>
     </div>
   );
 };
