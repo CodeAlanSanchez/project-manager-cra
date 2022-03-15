40384af36d0c216b16f92c21af86a8d34895b7a7
Alan Sanchez <realalansan@gmail.com>
2022-03-14 17:14:15 -0700

    Add header links

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,6 +1,30 @@
+import { Link } from 'react-router-dom';
+import 'styles/components/_header.scss';
 
 const Header: React.FC = () => {
-return <header>Header</header>;
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <ul className="list">
+          <li className="listItem">
+            <Link to={''}>Hira</Link>
+          </li>
+          <li className="listItem">
+            <Link to={''}>Home</Link>
+          </li>
+          <li className="listItem">
+            <Link to={'/projects'}>Projects</Link>
+          </li>
+          <li className="listItem">
+            <Link to={'/about-us'}>About</Link>
+          </li>
+          <li className="listItem">
+            <Link to={'/account'}>Account</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
