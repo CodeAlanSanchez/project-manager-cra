33f6015e36553a3f55d472d7bfa282bb47f6ccae
Alan Sanchez <realalansan@gmail.com>
2022-03-15 16:21:28 -0700

    Finish footer component

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -1,6 +1,39 @@
+import 'styles/components/_footer.scss';
 
 const Footer: React.FC = () => {
-return <footer>Footer</footer>;
+  return (
+    <footer className="footer">
+      <div className="content">
+        <div>
+          <p className="heading">HERE</p>
+          <a href="/register" className="link">
+            Register
+          </a>
+          <a href="/login" className="link">
+            Login
+          </a>
+          <a href="/home" className="link">
+            Home
+          </a>
+          <a href="/about-us" className="link">
+            About
+          </a>
+          <a href="/projects" className="link">
+            Projects
+          </a>
+        </div>
+        <div>
+          <p className="heading">ME</p>
+          <a href="https://www.alansanchez.dev" className="link">
+            Portfolio
+          </a>
+          <a href="https://www.github.com/CodeAlanSanchez" className="link">
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
