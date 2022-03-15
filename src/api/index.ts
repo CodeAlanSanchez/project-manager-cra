59bf6255a000fc373e7a379685d5bef66826fe7b
Alan Sanchez <realalansan@gmail.com>
2022-03-15 16:26:47 -0700

    Add basic routes for api

---
+++ src/api/index.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+import { API_URL } from 'config';
+
+const axiosInstance = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+});
+
+export const fetchProjects = () => axiosInstance.get(`api/project`);
+export const fetchProject = (id: number) =>
+  axiosInstance.get(`api/project/${id}`);
+export const createProject = (project: any) =>
+  axiosInstance.post(`api/project`, project);
+export const updateProject = (id: number, project: any) =>
+  axiosInstance.put(`api/project/${id}`, project);
+export const deleteProject = (id: number) =>
+  axiosInstance.delete(`api/project/${id}`);
+
+export const signin = (form: any) => axiosInstance.post(`api/login`, form);
+export const register = (form: any) => axiosInstance.post(`api/register`, form);
