d1a6161097d2d463f305ff4383a8fb481a9eab19
Alan Sanchez <realalansan@gmail.com>
2022-03-15 19:53:55 -0700

    Add placeholder div to keep styles across pages

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { Header } from './Header';
 import { Footer } from './Footer';
+import React from 'react';
 import 'styles/root/_layout.scss';
 
 interface Props {
@@ -11,6 +12,7 @@ const Layout: React.FC<Props> = ({ children }: Props) => {
   return (
     <div className={`layout`}>
       <Header />
+      <div className="placeholder" />
       {children}
       <Footer />
     </div>
