17b71c048eee06b3078f15276ba445d7ae456689
Alan Sanchez <realalansan@gmail.com>
2022-03-16 12:20:58 -0700

    Move project fetching to projects

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,23 @@
+import { getProjects } from 'actions/projectActions';
+import { Project } from 'components/Project';
+import { useAppDispatch, useAppSelector } from 'hooks';
+import { useEffect } from 'react';
 import 'styles/pages/_projects.scss';
 
 const Projects: React.FC = () => {
-  return <div className="projects"></div>;
+  const projects = useAppSelector((state) => state.projects);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getProjects());
+  }, [dispatch]);
+  return (
+    <div className="projects">
+      {projects.map((p: any) => (
+        <Project key={p.id} project={p} />
+      ))}
+    </div>
+  );
 };
 
 export default Projects;
