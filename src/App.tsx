7565ddca5da2ad9c54bae32d4b3fa0243f889996
Alan Sanchez <realalansan@gmail.com>
2022-03-16 11:55:29 -0700

    Fetch projects in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { fetchProjects } from 'api';
 import Layout from 'components/Layout/Layout';
+import { useAppDispatch } from 'hooks';
 import { Home, Projects } from 'pages';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App: React.FC = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchProjects());
+  }, [dispatch]);
+
   return (
     <Router>
       <Layout>
