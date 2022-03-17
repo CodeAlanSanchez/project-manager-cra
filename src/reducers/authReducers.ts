1cecb00f7ef4d7019b2ed39af03b1da66acafec2
Alan Sanchez <realalansan@gmail.com>
2022-03-16 22:40:47 -0700

    Add default case for auth

--- src/reducers/authReducers.ts
+++ src/reducers/authReducers.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default (user = {}, action: any) => {
-    switch (action.type) {
-    }
-  };
-  
+  switch (action.type) {
+    default:
+      return user;
+  }
+};
