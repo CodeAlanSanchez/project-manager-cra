dc53c6bcd301a8da6f0f16d0b58a5e19694c5474
Alan Sanchez <realalansan@gmail.com>
2022-03-16 22:36:00 -0700

    Add react-redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import Layout from 'components/Layout/Layout';
 import { Home, Projects } from 'pages';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import store from 'store';
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </Provider>
   );
 };
 
