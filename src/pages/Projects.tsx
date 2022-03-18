fce15f99429cd47067d2c85808db877de9b9bec6
Alan Sanchez <realalansan@gmail.com>
2022-03-18 16:10:43 -0700

    Add dialog component as project form

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,7 +1,10 @@
 import { getProjects } from 'actions/projectActions';
 import AuthForm from 'components/AuthForm';
+import Button from 'components/Button/Button';
+import Dialog from 'components/Dialog';
 import Error from 'components/Error';
 import { Project } from 'components/Project';
+import ProjectForm from 'components/ProjectForm';
 import { useAppDispatch, useAppSelector } from 'hooks';
 import { useEffect, useState } from 'react';
 import 'styles/pages/_projects.scss';
@@ -9,12 +12,17 @@ import 'styles/pages/_projects.scss';
 const Projects: React.FC = () => {
   const projects = useAppSelector((state) => state.projects);
   const [loading, setLoading] = useState(true);
+  const [showDialog, setShowDialog] = useState(false);
   const error = useAppSelector((state) => state.projectsError);
   const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(getProjects(setLoading));
   }, [dispatch]);
+
+  const handleClose = () => {
+    setShowDialog(false);
+  };
 
   if (error?.field) {
     return (
@@ -27,13 +35,26 @@ const Projects: React.FC = () => {
 
   return (
     <div className="projects">
-      {loading ? (
-        <div>Loading...</div>
-      ) : projects.length > 0 ? (
-        projects.map((p: any) => <Project key={p.id} project={p} />)
+      {showDialog ? (
+        <Dialog setVisible={setShowDialog}>
+          <ProjectForm onSubmit={handleClose} />
+        </Dialog>
       ) : (
-        <div>You have no projects, create one to get started!</div>
+        ''
       )}
+      <div className="content">
+        {loading ? (
+          <div>Loading...</div>
+        ) : projects.length > 0 ? (
+          projects.map((p: any) => <Project key={p.id} project={p} />)
+        ) : (
+          <div>You have no projects, create one to get started!</div>
+        )}
+        <Button sm rounded onClick={() => setShowDialog((prev) => !prev)}>
+          New Project
+        </Button>
+        <h1>{showDialog ? 'true' : 'false'}</h1>
+      </div>
     </div>
   );
 };
