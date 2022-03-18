52467d8b18a99652dffc20540060e9f5a708868e
Alan Sanchez <realalansan@gmail.com>
2022-03-18 00:28:39 -0700

    Add basic props to login form

--- src/components/AuthForm/Login/index.tsx
+++ src/components/AuthForm/Login/index.tsx
@@ -1,9 +1,14 @@
 interface Props {
+  register: Boolean;
   setRegister: Function;
   setVisible: Function;
 }
 
-const LoginForm: React.FC<Props> = ({ setRegister, setVisible }: Props) => {
+const LoginForm: React.FC<Props> = ({
+  register,
+  setRegister,
+  setVisible,
+}: Props) => {
   return <></>;
 };
 
