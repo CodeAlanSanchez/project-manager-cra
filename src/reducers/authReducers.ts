e686f128e09031852bfd565683facce359f558e0
Alan Sanchez <realalansan@gmail.com>
2022-03-20 18:05:12 -0700

    Add auth reducer options

--- src/reducers/authReducers.ts
+++ src/reducers/authReducers.ts
@@ -1,6 +1,11 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default (user = {}, action: any) => {
   switch (action.type) {
+    case 'LOGIN':
+      return action.payload;
+    case 'REGISTER':
+      return action.payload;
+    case 'GET_AUTH':
     default:
       return user;
   }
