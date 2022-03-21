c0d69f87a0056fbe960e146c4e8bed15c1eb68ea
Alan Sanchez <realalansan@gmail.com>
2022-03-20 18:03:39 -0700

    Add error catching and me action

--- src/actions/authActions.ts
+++ src/actions/authActions.ts
@@ -4,7 +4,7 @@ export const signIn = (body: any) => async (dispatch: any) => {
   try {
     const { data } = await api.signin(body);
 
-    dispatch({ type: 'LOGIN', payload: data });
+    dispatch({ type: 'LOGIN', payload: data.data });
   } catch (error: any) {
     const { field, message } = error.response.data.error;
     dispatch({
@@ -18,7 +18,21 @@ export const signUp = (body: any) => async (dispatch: any) => {
   try {
     const { data } = await api.register(body);
 
-    dispatch({ type: 'REGISTER', payload: data });
+    dispatch({ type: 'REGISTER', payload: data.data });
+  } catch (error: any) {
+    const { field, message } = error.response.data.error;
+    dispatch({
+      type: 'CREATE_AUTH_ERROR',
+      payload: { field, message },
+    });
+  }
+};
+
+export const me = () => async (dispatch: any) => {
+  try {
+    const { data } = await api.me();
+
+    dispatch({ type: 'LOGIN', payload: data });
   } catch (error: any) {
     const { field, message } = error.response.data.error;
     dispatch({
