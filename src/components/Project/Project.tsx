1198006c690927f02608645b3c27ad7db8282582
Alan Sanchez <realalansan@gmail.com>
2022-03-23 05:08:51 -0700

    Convert priority to text

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import 'styles/components/_project.scss';
 
 interface Props {
@@ -5,15 +6,27 @@ interface Props {
 }
 
 const Project: React.FC<Props> = ({ project }: Props) => {
+  const [priority, setPriority] = useState('');
+
+  useEffect(() => {
+    if (project.status === 1) {
+      setPriority('low');
+    } else if (project.status === 2) {
+      setPriority('medium');
+    } else if (project.status === 3) {
+      setPriority('high');
+    }
+  }, []);
+
   return (
     <div className="project">
       <div className="projectInfo">
         <h6 className="projectName">Name</h6>
         <p className="projectName">{project.name}</p>
       </div>
-      <div className="projectInfo">
-        <h6 className="projectStatus">Status</h6>
-        <p className="projectStatus">{project.status}</p>
+      <div className="projectInfo priority" >
+        <h6 className="projectPriority">Priority</h6>
+        <p className={`${priority} projectPriority`}>{priority}</p>
       </div>
       <div className="projectInfo">
         <h6 className="projectDescription">Description</h6>
