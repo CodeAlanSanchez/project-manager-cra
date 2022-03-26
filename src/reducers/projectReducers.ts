1091d863712908d9fd1bf1aed24ba01adcbc8eff
Alan Sanchez <realalansan@gmail.com>
2022-03-25 20:57:36 -0700

    Change strings to types

--- src/reducers/projectReducers.ts
+++ src/reducers/projectReducers.ts
@@ -1,18 +1,25 @@
+import {
+  CREATE_PROJECT,
+  DELETE_PROJECT,
+  FETCH_PROJECT,
+  FETCH_PROJECTS,
+  UPDATE_PROJECT,
+} from 'types/actions';
 /* eslint-disable import/no-anonymous-default-export */
 export default (projects = [], action: any) => {
   switch (action.type) {
     default:
       return projects;
-    case 'FETCH_PROJECTS':
-    case 'FETCH_PROJECT':
+    case FETCH_PROJECTS:
+    case FETCH_PROJECT:
       return action.payload;
-    case 'CREATE_PROJECT':
+    case CREATE_PROJECT:
       return [...projects, action.payload];
-    case 'UPDATE_PROJECT':
+    case UPDATE_PROJECT:
       return projects.map((project: any) =>
         project.id === action.payload.id ? action.payload : project
       );
-    case 'DELETE_PROJECT':
+    case DELETE_PROJECT:
       return projects.filter(
         (project: any) => project.id !== action.payload.id
       );
