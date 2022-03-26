e3daf83f5913f78ccc1164d2b1d403588e1964ce
Alan Sanchez <realalansan@gmail.com>
2022-03-25 19:45:21 -0700

    Create flexible form

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,43 @@
+import Button from 'components/Button/Button';
+import MyInput from 'components/Input';
+import { FormEvent, useState } from 'react';
+
+interface Props {
+  title: string;
+  keys: Array<any>;
+  onSubmit: Function;
+}
+
+const MyForm: React.FC<Props> = ({ title, keys, onSubmit }: Props) => {
+  const [form, setForm] = useState({
+    ...keys,
+  });
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit();
+  };
+
+  return (
+    <div>
+      <form className="myForm" onSubmit={(e) => handleSubmit(e)}>
+        <h5 className="heading">{title}</h5>
+        {keys.map((k) => (
+          <MyInput
+            name={k}
+            label={k}
+            placeholder={k}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setForm({ ...form, [`${k}`]: e.target.value })
+            }
+          ></MyInput>
+        ))}
+        <Button type="submit" styles={{ margin: '0 0 2rem' }} rounded sm>
+          Submit
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default MyForm;
