58bd626b30a993096b40dd4cef923cb0547e32b6
Alan Sanchez <realalansan@gmail.com>
2022-03-28 05:34:57 -0700

    Fix layout for fields row

--- src/components/Table/Row/Row.tsx
+++ src/components/Table/Row/Row.tsx
@@ -11,16 +11,20 @@ const Row: React.FC<Props> = ({ fields, properties, view }: Props) => {
     <div className={`${fields && 'fields'} row`}>
       {Object.values(properties).map((p: any) => {
         return (
-          <div key={p} className="rowItem">
+          <div
+            key={p + '__' + Math.random() * 100000000000000}
+            className="rowItem"
+          >
             <p>{p}</p>
           </div>
         );
       })}
-      {view && (
-        <div className="rowItem">
+      {view && !fields && (
+        <a href={`/projects/${properties.id}`} className="rowItem">
           <Button link>View&nbsp;{'>'}</Button>
-        </div>
+        </a>
       )}
+      {view && fields && <div></div>}
     </div>
   );
 };
