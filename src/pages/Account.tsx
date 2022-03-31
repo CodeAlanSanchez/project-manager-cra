69f71d8c57fb91812cf8b7fa1facf2e533c3654f
Alan Sanchez <realalansan@gmail.com>
2022-03-30 22:21:23 -0700

    Add account page if logged in

--- src/pages/Account.tsx
+++ src/pages/Account.tsx
@@ -1,10 +1,21 @@
+import { me } from 'actions/authActions';
+import FullAccount from 'components/Account';
 import AuthForm from 'components/AuthForm';
+import { useAppDispatch, useAppSelector } from 'hooks';
+import { useEffect } from 'react';
 import 'styles/pages/_account.scss';
 
 const Account: React.FC = () => {
+  const auth = useAppSelector((state) => state.auth);
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(me());
+  }, []);
+
   return (
     <div className="account">
-      <AuthForm />
+      {!auth.email && <AuthForm />} {auth.email && <FullAccount />}
     </div>
   );
 };
