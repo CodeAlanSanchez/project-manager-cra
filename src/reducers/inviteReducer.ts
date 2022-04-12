664740237d44d014c29abee1b8cd933f183f3c1b
Alan Sanchez <realalansan@gmail.com>
2022-04-12 12:13:51 -0700

    Add reducers for invites

--- src/reducers/inviteReducer.ts
+++ src/reducers/inviteReducer.ts
@@ -1,13 +1,20 @@
 import {
-    ACCEPT_INVITE,
-    DECLINE_INVITE,
-    SEND_INVITE,
-  } from 'types/actions';
-  /* eslint-disable import/no-anonymous-default-export */
-  export default (bugs = [], action: any) => {
-    switch (action.type) {
-      default:
-        return bugs;
-    }
-  };
-  
+  ACCEPT_INVITE,
+  DECLINE_INVITE,
+  FETCH_INVITE,
+  SEND_INVITE,
+} from 'types/actions';
+/* eslint-disable import/no-anonymous-default-export */
+export default (invites = [], action: any) => {
+  switch (action.type) {
+    case ACCEPT_INVITE:
+    case DECLINE_INVITE:
+      return invites.filter((i: any) => i.id !== action.payload.id);
+    case SEND_INVITE:
+      return [...invites, action.payload];
+    case FETCH_INVITE:
+      return action.payload;
+    default:
+      return invites;
+  }
+};
