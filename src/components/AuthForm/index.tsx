d2b4cf9a0d00ecafd32036090fa7c9be941c7f0d
Alan Sanchez <realalansan@gmail.com>
2022-04-18 21:41:36 +0000

    Add way to see login form first

--- src/components/AuthForm/index.tsx
+++ src/components/AuthForm/index.tsx
@@ -1,10 +1,31 @@
 import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import LoginForm from './Login';
 import RegisterForm from './Register';
 
 const AuthForm: React.FC = () => {
   const [register, setRegister] = useState(true);
   const [visible, setVisible] = useState(false);
+  const location = useLocation();
+
+  if (location?.hash === "#login" || location?.hash == "#login=") {
+    return register ? (
+      <LoginForm
+        register={register}
+        setRegister={setRegister}
+        visible={visible}
+        setVisible={setVisible}
+      />
+    ) : (
+      <RegisterForm
+        register={register}
+        setRegister={setRegister}
+        visible={visible}
+        setVisible={setVisible}
+      />
+    );
+  }
+
   return register ? (
     <RegisterForm
       register={register}
